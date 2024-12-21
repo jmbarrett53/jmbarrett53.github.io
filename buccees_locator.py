0f061ed43ce38cabc2fd9461897b2a6103447573
jmbarrett53 <jack@thebarretts.org>
2024-12-20 21:41:25 -0500

    Update buccees_locator.py

--- buccees_locator.py
+++ buccees_locator.py
@@ -1,5 +1,6 @@
 import csv
 import sys
+import math
 
 location_tuples = []
 with open('BuceesLocations.csv', mode='r') as file:
@@ -12,7 +13,28 @@ with open('BuceesLocations.csv', mode='r') as file:
 
         location_tuples.append((lattitude, longitude))
 
+def get_dist_to_nearest_buccees(lat_in, long_in):
+    """
+    Given the current location, return the smallest euclidean distance to a buccees
+    """
+    min_dist = float('inf')
+    for buccees_loc in location_tuples:
+        buc_lattitude = buccees_loc[0]
+        buc_longitude = buccees_loc[1]
+
+        # Get differences
+        delta_lat = lat_in - buc_lattitude
+        delta_long = long_in - buc_longitude
+
+        # Convert differences to miles
+        x = 69 * delta_lat
+        y = 69 * math.cos(math.radians((lat_in + buc_lattitude) / 2)) * delta_long
+        # Euclidean distance in miles
+        dist = math.sqrt(x**2 + y**2)
+        if dist < min_dist:
+            min_dist = dist
+    return min_dist
+
 print('Current Latitude is: ' + sys.argv[1])
 print('Current Longitude is: ' + sys.argv[2])
-print(location_tuples)
-# Now calculate the euclidean distance between you
+print('You are ' + get_dist_to_nearest_buccees(sys.argv[1], sys.argv[2]) + ' miles away from the nearest Buc-ees')
