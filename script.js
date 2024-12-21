6016f710c5f2669704c150ce3c774f29a485fa3a
jmbarrett53 <jack@thebarretts.org>
2024-12-21 13:19:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ async function getLocation() {
       userLatitude = position.coords.latitude;
       userLongitude = position.coords.longitude;
 
-      // Display map
-      displayMap(userLatitude, userLongitude);
+      // // Display map
+      // displayMap(userLatitude, userLongitude);
 
       // Send location to backend
       const response = await fetch('http://localhost:3000/location', {
@@ -16,7 +16,7 @@ async function getLocation() {
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ latitude: userLatitude, longitude: userLongitude }),
       });
-      // Next line will return as a string
+      // Next line will return as a string e.g., "('34.27238083', '-79.70439911')"
       const pyOutputStr = await response.text();
       console.log('Python Output:', pyOutputStr);
 
@@ -25,27 +25,39 @@ async function getLocation() {
 
       // Add the new marker to the map
       if (coordinates) {
-        addMarkerToMap(coordinates[0], coordinates[1]);
+        const bucLat = coordinates[0];
+        const bucLong = coordinates[1];
       }
-
+      else {
+        console.log("Invalid Coordinates")
+        return
+      }
+      
+      // Display map after calculating closest buc-ees
+      displayMap(userLatitude, userLongitude, bucLat, bucLong);
     });
   } else {
     console.log("Geolocation is not supported by this browser.");
   }
 }
 
-function displayMap(lat, lon) {
-  var map = L.map('map').setView([lat, lon], 13);
+function displayMap(userLat, userLong, bucLat, bucLong) {
+  var map = L.map('map').setView([userLat, userLong], 13);
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; OpenStreetMap contributors',
   }).addTo(map);
 
-  L.marker([lat, lon]).addTo(map)
+  // Add current location
+  L.marker([userLat, userLong]).addTo(map)
     .bindPopup('Your Location')
+    .openPopup();
+
+    L.marker([bucLat, bucLong]).addTo(map)
+    .bindPopup("Closest Buc-ee's")
     .openPopup();
 }
 
-// Function to parse tuple string into an array of numbers
+// Function to parse tuple string e.g., "('34.27238083', '-79.70439911')" into an array of numbers
 function parseCoordinates(tupleStr) {
   try {
     // Remove parentheses and split by comma
